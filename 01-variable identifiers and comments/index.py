a40e0d5c4b7ba5fcb26ede54d1f3abe6ffabc4c5
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-04-03 18:31:30 +0530

    01 variable identifiers and comments

---
+++ 01-variable identifiers and comments/index.py
@@ -0,0 +1,14 @@
+# Thie is comment in python - Lerning Python for Generative AI
+
+age = 20
+age = 200
+age = "Hello"
+print(age)
+
+#Identifier -> meaningful name given to variable is known as Identifier
+#There are rules to define Identifiers
+
+#start with (a-z, A-Z) or underscore(_)
+#remaining characters can be letters, underscores or digits(0-9)
+#case sensitive means -> name and Name are different
+#reserved keywords are not allowed -> while, if, print etc
