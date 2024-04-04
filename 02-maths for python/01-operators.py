011ec9d61694192684a419d115428efc13cfd2f3
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-04-04 14:22:48 +0530

    maths for python

---
+++ 02-maths for python/01-operators.py
@@ -0,0 +1,49 @@
+#Addition
+add = 5 + 3
+print(add)
+
+#Substraction
+diff = 10 - 3
+print(diff)
+
+#Multiplication
+mul = 10*10
+print(mul)
+
+#Division
+divWithDecimal = 10/2
+divWithoutDecimal = 10//2
+ 
+print(divWithDecimal) #5.0
+print(divWithoutDecimal) #5
+
+#Exponentiation
+power_result = 2**3 # 2 * 2 * 2
+print(power_result)
+
+
+#Modulo
+mod = 38 % 8
+print(mod)
+
+#Assignment Operator
+one = 10
+one += 10
+print(one)
+
+two = 10
+two -= 10
+print(two)
+
+three = 10
+three *= 2
+print(three)
+
+four = 10
+four /= 2
+print(four)
+
+
+five = 100
+five %= 8
+print(five)
