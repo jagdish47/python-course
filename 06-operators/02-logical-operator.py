680a66ec5cf70ac72b74718792a42ca092a2b9a0
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-04-10 15:15:10 +0530

    Operators in Python

---
+++ 06-operators/02-logical-operator.py
@@ -0,0 +1,20 @@
+# and &
+# everyone should be true than it's true otherwise false
+
+# True & True & True = True
+# True and True and False = False
+
+
+# or | 
+# if one of them is true it is true
+
+# True or True = True
+# True or True or False = True
+# False or False = False
+
+
+# not !
+# it make true to false and false to true
+
+# not True = false
+# not false = true
