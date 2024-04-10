17fa80a644e6f14f8cbf2f9cb0e8302266bb6f97
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-04-10 14:23:11 +0530

    boolean and typecasting

---
+++ 04-boolean and type casting/index.py
@@ -0,0 +1,8 @@
+x = True
+print(x)
+
+y = False
+print(y)
+
+#checking the which type of data
+print(type(y))
---
+++ 04-boolean and type casting/type-casting.py
@@ -0,0 +1,13 @@
+#converting one datatype to another datatype
+
+#int() -> convert floating number into int
+float_num = 55.5555
+
+print(type(float_num))
+
+convert_num = int(float_num)
+print(type(convert_num))
+
+#float() -> converting int to float
+#str() -> convert other data type to string
+#bool() -> converting into bool
